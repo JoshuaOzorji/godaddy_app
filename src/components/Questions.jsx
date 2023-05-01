27aaeaaf8e580a223f909764db3c4c1175038af9
JoshuaOzorji <ozorjijoshua@gmail.com>
2023-05-01 15:58:58 +0100

    error

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,27 +2,26 @@ import React, { useState } from "react";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
 const Questions = ({ title, content }) => {
-  const [showInfo, setShowInfo] = useState(false);
-  return (
-    <article className="my-4">
-      <header className="flex justify-between max-w-[47rem]">
-        <h4 className="w-[47rem] font-bold font-lato md:text-2xl sm:py-8 border-t md:pt-6 sm:pb-3 tracking-wide">
-          {title}
-        </h4>
-        <button
-          className="flex max-w-[47rem] h-[2rem] bg-yellow items-center justify-center rounded-lg text-black cursor-pointer ml-4 self-center"
-          onClick={() => setShowInfo(!showInfo)}
-        >
-          {showInfo ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
-        </button>
-      </header>
-      {showInfo && (
-        <p className="max-w-[47rem] leading-loose text-base font-lato font-light md:pt-3 md:pr-36 sm:w-[90%] pb-6">
-          {content}
-        </p>
-      )}
-    </article>
-  );
+	const [showInfo, setShowInfo] = useState(false);
+	return (
+		<article className="my-4">
+			<header className="flex justify-between max-w-[47rem]">
+				<h4 className="w-[47rem] font-bold font-lato md:text-2xl sm:py-8 border-t md:pt-6 sm:pb-3 tracking-wide">
+					{title}
+				</h4>
+				<button
+					className="flex max-w-[47rem] h-[2rem] bg-yellow items-center justify-center rounded-lg text-black cursor-pointer ml-4 self-center transition-all duration-300"
+					onClick={() => setShowInfo(!showInfo)}>
+					{showInfo ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+				</button>
+			</header>
+			{showInfo && (
+				<p className="max-w-[47rem] leading-loose text-base font-lato font-light md:pt-3 md:pr-36 sm:w-[90%] pb-6 transition-all duration-300">
+					{content}
+				</p>
+			)}
+		</article>
+	);
 };
 
 export default Questions;
